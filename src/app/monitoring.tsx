688f0c7fa51e164c61b66da3506fb95c92caeb9e
6740011066-droid <6740011066@psu.ac.th>
2026-08-06 10:32:17 +0700

    Add comment placeholder in MonitoringDashboard function

--- src/app/monitoring.tsx
+++ src/app/monitoring.tsx
@@ -17,6 +17,7 @@ export default function MonitoringDashboard() {
     });
     return sub;
   };
+  //hfgfdh dtjgjdfyffr
 
   // ===== ฟังก์ชันดึงข้อมูลจาก API =====
   const fetchMonitoring = async () => {
